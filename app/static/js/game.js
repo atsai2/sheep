ce8fafd7f91b5b44e0911aa8210f30aa9cc53162
Jeff Ou <jou60@stuy.edu>
2026-01-13 06:00:11 -0500

    More experimentation + devlog

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,12 +1,35 @@
 function draw_table(){
-  const canvas = document.getElementById("canvas1");
-  const ctx = canvas.getContext("2d");
+  ctx.beginPath() //table
+  ctx.ellipse(500, 220, 400, 200, 0, 0, 2 * Math.PI)
+  ctx.stroke()
 
   ctx.beginPath()
-  ctx.ellipse(500, 250, 400, 200, 0, 0, 2 * Math.PI)
-  ctx.stroke()
+  ctx.arc(75, 425, 50, 0, Math.PI * 2, true);
+  ctx.fillStyle = "#A3F0A3";
+  ctx.fill()
+}
+
+function change_button(){
+  ctx.beginPath()
+  ctx.arc(75, 425, 50, 0, Math.PI * 2, true);
+  ctx.fillStyle = "green";
+  ctx.fill()
 }
 
 
 //RUNNING CODE
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
 draw_table()
+
+canvas.addEventListener('click', function(event) {
+  console.log(event.clientX, event.clientY)
+})
+
+canvas.addEventListener('mouseover', function(event) {
+  console.log("mouseover " + event.clientX, event.clientY)
+  change_button()
+})
+
